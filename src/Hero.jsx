a8dd7fb18e80e651a21311eb767b4d739b76eb8a
Houjun Liu <hliu@shabang.cf>
2020-11-27 19:26:54 -0800

    top hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,6 +1,20 @@
+import './Hero.css';
+
 function Hero() {
     return (
         <div id="top-hero">
+            <div id="top-hero-top-alignment">
+                <div id="top-hero-top-part">
+                    <div id="top-hero-callout">
+                        Task Management, Simplified.
+                    </div>
+                    <div id="top-hero-title">
+                        The Condution Project
+                    </div>
+                </div>
+            </div>
+            <div id="top-hero-actions">
+            </div>
         </div>
     );
 }
