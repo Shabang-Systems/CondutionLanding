ea8004eecbae821e72e31fc7f466617b843c4a78
Houjun Liu <hliu@shabang.cf>
2020-11-27 18:25:08 -0800

    initial website commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from './Hero';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero() {
+    return (
+        <div id="top-hero">
+        </div>
+    );
+}
+
+export default Hero;
+
