81c4f83470fe1ba1846a32fcfdf3d42f98aface0
Houjun Liu <hliu@shabang.cf>
2020-11-29 09:08:31 -0800

    add submodules

--- src/Downloads.jsx
+++ src/Downloads.jsx
@@ -11,7 +11,7 @@ function Downloads(props) {
                 <span className="downloads-label">On your computer...</span>
                 <br />
                 <a className="downloads-button" href="https://drive.google.com/uc?export=download&id=1a7OpQcpqU1uIChAbe_-BfzfsqdKYz1AL"><i class="fab fa-apple" /> macOS</a>
-                <a className="downloads-button" href="https://drive.google.com/uc?export=download&id=1Aq2oCMhWotyZHiZTbLRwqvWMwrvnEogv"><i class="fab fa-windows" /> Windows</a>
+                <a className="downloads-button" href="https://drive.google.com/uc?export=download&id=1jAfmFoAuHjuaDNTBp-H3WGxLpO2uLhee"><i class="fab fa-windows" /> Windows</a>
                 <a className="downloads-button"><i class="fab fa-linux" onClick={()=>alert("Coming soon... Seriously")}/> Linux</a>
                 <a className="downloads-button" href="https://beta.condution.com"><i class="fas fa-globe" /> Web</a>
             </div>
@@ -21,13 +21,10 @@ function Downloads(props) {
                 <a className="downloads-button" href="https://testflight.apple.com/join/BPsiQBKV"><i class="fab fa-apple" /> iOS</a>
                 <a className="downloads-button" href="https://play.google.com/store/apps/details?id=cf.shabang.condution"><i class="fab fa-android" /> Android</a>
             </div>
-            <div className="downloads-paddingbox hash">
+            <div className="downloads-paddingbox hash" style={{display: "inline-block"}}>
                 <div className="downloads-hashbox">
-                    <div className="downloads-filename">Condution_beta-v1.0.0_win64 <div className="downloads-hash">30e547b527a2db57d20f81cde94c761c8f622f3d9adbdfb910f44b2baab2c44b</div></div>
-                    <div className="downloads-filename">Condution_beta-v1.0.0_win64 <div className="downloads-hash">30e547b527a2db57d20f81cde94c761c8f622f3d9adbdfb910f44b2baab2c44b</div></div>
-                </div>
-                <div className="downloads-hashbox">
-                    <div className="downloads-filename">Condution_beta-v1.0.0_win64 <div className="downloads-hash">30e547b527a2db57d20f81cde94c761c8f622f3d9adbdfb910f44b2baab2c44b</div></div>
+                    <div className="downloads-filename">Condution_beta-v1.0.0_darwin <div className="downloads-hash">968d4f95d46f3fa36a1452592f9da2d5221bca4e5884def15081893e55c625f2</div></div>
+                    <div className="downloads-filename">Condution_beta-v1.0.0_win64 <div className="downloads-hash">a8f64ff4870654651f58a203396bd77777eb5d1c1aeb313dcf0d96dc5b8e560f</div></div>
                     <div className="downloads-filename">Condution_beta-v1.0.0_win64 <div className="downloads-hash">30e547b527a2db57d20f81cde94c761c8f622f3d9adbdfb910f44b2baab2c44b</div></div>
                 </div>
             </div>
