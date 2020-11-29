1e3a94dbe83c9375dd60b0b4ea8bf49a74038b40
Houjun Liu <hliu@shabang.cf>
2020-11-28 18:07:42 -0800

    Bottom panel. Nice!

--- src/Downloads.jsx
+++ src/Downloads.jsx
@@ -22,8 +22,6 @@ function Downloads(props) {
                 <div className="downloads-button"><i class="fab fa-android" /> Android</div>
             </div>
             <div className="downloads-paddingbox hash">
-                <span className="downloads-label">Some hashes for nerds (like us):</span>
-                <br />
                 <div className="downloads-hashbox">
                     <div className="downloads-filename">Condution_beta-v1.0.0_win64 <div className="downloads-hash">30e547b527a2db57d20f81cde94c761c8f622f3d9adbdfb910f44b2baab2c44b</div></div>
                     <div className="downloads-filename">Condution_beta-v1.0.0_win64 <div className="downloads-hash">30e547b527a2db57d20f81cde94c761c8f622f3d9adbdfb910f44b2baab2c44b</div></div>
@@ -32,6 +30,16 @@ function Downloads(props) {
                     <div className="downloads-filename">Condution_beta-v1.0.0_win64 <div className="downloads-hash">30e547b527a2db57d20f81cde94c761c8f622f3d9adbdfb910f44b2baab2c44b</div></div>
                     <div className="downloads-filename">Condution_beta-v1.0.0_win64 <div className="downloads-hash">30e547b527a2db57d20f81cde94c761c8f622f3d9adbdfb910f44b2baab2c44b</div></div>
                 </div>
+            </div>
+            <div style={{textAlign: "center"}}>
+                <ul class="icons">
+                    <li><a href="https://twitter.com/Condution" class="fab fa-twitter"></a></li>
+                    <li><a href="https://patreon.com/condution" class="fab fa-patreon"></a></li>
+                    <li><a href="mailto:support@shabang.cf" class="fas fa-envelope"></a></li>
+                    <li><a href="https://github.com/Shabang-Systems/Condution" class="fab brands fa-github"></a></li>
+                    <li><a href="https://www.youtube.com/channel/UCHoAJexhqGJwIEOD3RELBlw" class="fab fa-youtube"></a></li>
+                    <li><a href="https://discord.gg/UaaqueY" class="fab fa-discord"></a></li>
+                </ul>
             </div>
         </div>
     )
--- src/Story.jsx
+++ src/Story.jsx
@@ -14,9 +14,6 @@ function Story(props) {
             <div className="chapter">
                 <h1 className="story-header story-callout brown">Flexible <span className="story-callout invbrown">Date Handling</span></h1>
                 <br />
-                    {/*<animated.div className="image-annotation" style={{opacity:largeCondutionSpring.opacity1p1}}>
-                    Due and Defer dates + times
-                </animated.div>*/}
                 <animated.img className="story-image story-image-1-1" src={largeDemo} alt={"Large Condution Image"} style={{transform: interpolate(largeCondutionSpring.scale1p1, (c=>`translateX(${c}vw)`))}}/>
                 <div className="story-description">Due and Start Dates <span className="invbrown" style={{paddingLeft: 5, paddingRight: 5}}>down to the second.</span></div>
             </div>
